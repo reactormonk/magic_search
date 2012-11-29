43757722ea59d80eddc04145c1a656cb47ca3cad
Simon Hafner <hafnersimon@gmail.com>
2012-11-29 01:04:55 -0600

    moved setup of html test

--- mana-symbols/test/html.rb
+++ mana-symbols/test/html.rb
@@ -6,14 +6,13 @@ def normalize(html)
 end
 
 describe ManaSymbols do
+  ManaSymbols.image_location = "/foo/bar"
   it "should return them as img tags" do
-    ManaSymbols.image_location = "/foo/bar"
     normalize(ManaSymbols::parse("{R}").to_html).must_equal normalize("<img class='mana-symbol' src='/foo/bar/R.svg' alt='R'/>")
     normalize(ManaSymbols::parse("{R}{W}").to_html).must_equal normalize("<img class='mana-symbol' src='/foo/bar/R.svg' alt='R'/><img class='mana-symbol' src='/foo/bar/W.svg' alt='W'/>")
     normalize(ManaSymbols::parse("{2}{W}").to_html).must_equal normalize("<img class='mana-symbol' src='/foo/bar/2.svg' alt='2'/><img class='mana-symbol' src='/foo/bar/W.svg' alt='W'/>")
   end
   it "should return them as img tags" do
-    ManaSymbols.image_location = "/foo/bar"
     normalize(ManaSymbols::parse("{R/P}").to_html).must_equal normalize("<img class='mana-symbol' src='/foo/bar/R-P.svg' alt='R/P'/>")
     normalize(ManaSymbols::parse("{R/W}{U/B}").to_html).must_equal normalize("<img class='mana-symbol' src='/foo/bar/R-W.svg' alt='R/W'/><img class='mana-symbol' src='/foo/bar/U-B.svg' alt='U/B'/>")
   end
